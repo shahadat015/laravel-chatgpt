e5ba96161e746f7de12a43e43ff8285cfcd1a3ba
Shahadat Hossain <shahadat015@yahoo.com>
2022-12-28 11:01:40 +0600

    code refactor and some ui improvement

--- resources/js/App.vue
+++ resources/js/App.vue
@@ -4,7 +4,7 @@
     >
         <div class="max-w-6xl w-full mx-auto sm:px-6 lg:px-8 space-y-4 py-4">
             <div class="text-center text-gray-800 dark:text-gray-300 py-4">
-                <h1 class="text-7xl font-bold">Writebot</h1>
+                <h1 class="text-7xl font-bold">ChatBot</h1>
             </div>
 
             <div
@@ -45,7 +45,20 @@
                                 d="M4 12a8 8 0 018-8V0C5.373 0 0 5.373 0 12h4zm2 5.291A7.962 7.962 0 014 12H0c0 3.042 1.135 5.824 3 7.938l3-2.647z"
                             ></path>
                         </svg>
-                        {{ loading ? "Generating" : "Generate" }}
+                        <svg
+                            v-else
+                            class="-ml-1 mr-3 h-5 w-5 text-white"
+                            xmlns="http://www.w3.org/2000/svg"
+                            viewBox="0 0 512 512"
+                            stroke="currentColor"
+                        >
+                            <path
+                                fill="currentColor"
+                                d="M498.1 5.6c10.1 7 15.4 19.1 13.5 31.2l-64 416c-1.5 9.7-7.4 18.2-16 23s-18.9 5.4-28 1.6L284 427.7l-68.5 74.1c-8.9 9.7-22.9 12.9-35.2 8.1S160 493.2 160 480V396.4c0-4 1.5-7.8 4.2-10.7L331.8 202.8c5.8-6.3 5.6-16-.4-22s-15.7-6.4-22-.7L106 360.8 17.7 316.6C7.1 311.3 .3 300.7 0 288.9s5.9-22.8 16.1-28.7l448-256c10.7-6.1 23.9-5.5 34 1.4z"
+                            />
+                        </svg>
+
+                        {{ loading ? "Asking" : "Ask" }}
                     </button>
                 </form>
             </div>
@@ -77,7 +90,6 @@ let handleSubmit = () => {
         })
         .then(function (response) {
             loading.value = false;
-            title.value = response.data.title;
             content.value = response.data.content;
         })
         .catch(function (error) {
